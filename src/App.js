db9040594da9f6f99c518da2c52f18cb2e743759
bengin34 <becaglar3434@gmail.com>
2023-03-09 14:12:43 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {createUserWithEmailAndPassword , onAuthStateChanged, signInWithEmailAndPassword, signOut} from "firebase/auth"
+import {auth} from './auth/firebase-config'
+
+
+function App() { 
+
+//  user logged in : {user.email/password}
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
